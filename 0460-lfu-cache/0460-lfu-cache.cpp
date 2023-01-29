80914ced707b6c89dbf8b01efa0df51ddbe2aa4c
Deepanshu Kamboj <78980433+deepkamboj07@users.noreply.github.com>
2023-01-29 23:13:42 +0530

    Time: 557 ms (65.05%), Space: 175.3 MB (95.93%) - LeetHub

---
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -0,0 +1,35 @@
+class LFUCache {
+public:
+    priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+    unordered_map<int,int> m;
+    unordered_map<int,int> freq;
+    int cap, t = 0;
+    LFUCache(int capacity) {
+        cap = capacity;
+    }
+    
+    int get(int key) {
+        if(m.find(key)==m.end()) return -1;
+        put(key,m[key]);
+        return m[key];
+    }
+    
+    void put(int key, int value) {
+        if(cap==0){
+            return;
+        }
+        if(m.find(key)==m.end()&&m.size()==cap){
+            while(m.size()==cap){
+                auto x = pq.top();
+                if(freq[x.second.second]==x.first){
+                    m.erase(x.second.second);
+                    freq.erase(x.second.second);
+                }
+                pq.pop();
+            }            
+        }
+        m[key] = value;
+        freq[key]++;
+        pq.push({freq[key],{t++,key}});
+    }
+};
