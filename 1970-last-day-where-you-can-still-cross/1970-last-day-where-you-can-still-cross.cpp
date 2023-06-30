91a109ded3a54c2faea67b4bdba0e20d5e575df7
Deepanshu Kamboj <78980433+deepkamboj07@users.noreply.github.com>
2023-06-30 17:37:38 +0530

    Time: 640 ms (54.95%), Space: 171 MB (54.50%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     bool check(int i,int j,vector<vector<int>>&v,int r,int c)
     {   
         if(i<0 || i>=r || j<0 || j>=c || v[i][j]==1 || v[i][j]==2)
@@ -13,34 +14,41 @@ public:
         bool d=check(i,j-1,v,r,c);
         return a||b||c1||d;
     }
-    bool solve(vector<vector<int>>&v,vector<vector<int>>&w,int m)
+    
+    
+    bool solve(int mid, int col,int row, vector<vector<int>> &grid, vector<vector<int>>&w)
     {
-        for(int i=0;i<m;i++)
-            v[w[i][0]-1][w[i][1]-1]=1;
-        int r=v.size(),c=v[0].size();
-        for(int i=0;i<c;i++)
-        {   
-            if(v[0][i]==0 && check(0,i,v,r,c))
-                return 1;
+            
+        for(int i=0;i<mid;i++)
+            grid[w[i][0]-1][w[i][1]-1]=1;
+    
+        for(int i=0;i<col;i++)
+        {
+            if(grid[0][i]==0 && check(0,i,grid,row,col))return true;
         }
-        return 0;
+        
+        return false;
     }
-    int latestDayToCross(int r, int c, vector<vector<int>>&w) {
-        int n=w.size();
-        int l=0,h=n-1;
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
         int ans=0;
-        while(l<=h)
-        {   
-            vector<vector<int>>v(r,vector<int>(c,0));
-            int m=(l+h)/2;
-            if(solve(v,w,m))
+        int n=cells.size();
+        int low=0,high=n-1;
+        
+        
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            vector<vector<int>> grid(row,vector<int>(col,0));
+                  
+            if(solve(mid,col,row,grid,cells))
             {
-                ans=m;
-                l=m+1;
+                ans=mid;
+                low=mid+1;
             }
-            else
-                h=m-1;
+            else high=mid-1;    
+            
         }
+        
         return ans;
     }
 };
